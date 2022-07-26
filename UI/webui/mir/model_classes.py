d9c2ceb22dd37f8fe5b980458cef44a63eaa22b4
Arshan <arshandalili@gmail.com>
2022-07-26 10:51:25 +0430

    Add init and models

---
+++ UI/webui/mir/model_classes.py
@@ -0,0 +1,114 @@
+import fasttext
+import os
+import numpy as np
+import hazm
+import re
+from string import punctuation
+
+
+class Preprocessor:
+
+    def __init__(self, stopwords_path):
+        self.stopwords = []
+        with open(stopwords_path, encoding='utf-8') as file:
+            self.stopwords = file.read().split()
+
+    def preprocess(self, text):
+        text = self.normalize(text)
+        text = self.remove_links(text)
+        text = self.remove_punctuations(text)
+        words = self.word_tokenize(text)
+        words = self.remove_invalid_words(words)
+        words = self.remove_stopwords(words)
+        return words
+
+    def normalize(self, text):
+        return hazm.Normalizer().normalize(text)
+
+    def remove_links(self, text):
+        patterns = ['\S*http\S*', '\S*www\S*', '\S+\.ir\S*', '\S+\.com\S*', '\S+\.org\S*', '\S*@\S*']
+        for pattern in patterns:
+            text = re.sub(pattern, ' ', text)
+        return text
+
+    def remove_punctuations(self, text):
+        return re.sub(f'[{punctuation}؟،٪×÷»«]+', '', text)
+
+    def word_tokenize(self, text):
+        return hazm.word_tokenize(text)
+
+    def remove_invalid_words(self, words):
+        return [word for word in words if len(word) > 3 or re.match('^[\u0600-\u06FF]{2,3}$', word)]
+
+    def remove_stopwords(self, words):
+        return [word for word in words if word not in self.stopwords]
+
+
+
+
+
+
+class FastText:
+
+    def __init__(self, preprocessor=None, method='skipgram'):
+        self.method = method
+        self.mean_embed = []
+        self.model = None
+        self.preprocessor = preprocessor
+
+    def train(self, texts):
+        with open('FastText_train.txt', 'w', encoding='utf-8') as file:
+            file.write('\n'.join(list(map(lambda doc: ' '.join(doc), texts))))
+        self.model = fasttext.train_unsupervised('FastText_train.txt', self.method, minn=2, maxn=5, wordNgrams=10)
+        os.remove('FastText_train.txt')
+        self.mean_embed = list(map(lambda doc:
+                                   np.mean(list(map(lambda word:
+                                                    self.model.get_word_vector(word), doc)), axis=0), texts))
+        self.mean_embed = np.array(self.mean_embed)
+
+    def predict(self, query, dataset, k):
+        if self.preprocessor:
+            query = self.preprocessor.preprocess(query)
+        if type(query) == str:
+            query = query.split()
+        query_embed = np.mean(list(map(lambda word: self.model.get_word_vector(word), query)), axis=0)
+        dataset_sim = np.array(list(map(lambda doc: self.cosine_sim(query_embed, doc), self.mean_embed)))
+        idx = np.argsort(-dataset_sim)
+        return dataset.iloc[list(idx[:k])]
+
+    def expand_query(self, query, k=5, lambda_0=1, lambda_1=1):
+        if self.preprocessor:
+            query = self.preprocessor.preprocess(query)
+        if type(query) == str:
+            query = query.split()
+        query_embed = np.mean(list(map(lambda word: self.model.get_word_vector(word), query)), axis=0)
+        dataset_sim = np.array(list(map(lambda doc: self.cosine_sim(query_embed, doc), self.mean_embed)))
+        idx = np.argsort(-dataset_sim)
+        relevant_docs_mean = np.mean(self.mean_embed[list(idx[:k]), :], axis=0)
+        irrelevant_docs_mean = np.mean(self.mean_embed[list(idx[-k:]), :], axis=0)
+        final_embed = query_embed + lambda_0 * relevant_docs_mean - lambda_1 * irrelevant_docs_mean
+        return final_embed
+    
+    def predict_query_vector(self, query_embed, dataset, k):
+        dataset_sim = np.array(list(map(lambda doc: self.cosine_sim(query_embed, doc), self.mean_embed)))
+        idx = np.argsort(-dataset_sim)
+        return dataset.iloc[list(idx[:k])]
+
+    def cosine_sim(self, query, doc):
+        return np.dot(query, doc) / (np.linalg.norm(query) * np.linalg.norm(doc))
+
+    def save_FastText_model(self, path='models/FastText_model.bin'):
+        self.model.save_model(path)
+        np.save('models/FastText_mean_embed.npy', self.mean_embed)
+
+    def load_FastText_model(self, path="models/FastText_model.bin"):
+        self.model = fasttext.load_model(path)
+        self.mean_embed = np.load('models/FastText_mean_embed.npy')
+
+    def prepare(self, dataset, mode, save=False):
+        if mode == 'train':
+            self.train(dataset)
+        if mode == 'load':
+            self.load_FastText_model()
+        if save:
+            self.save_FastText_model()
--- UI/webui/mir/views.py
+++ UI/webui/mir/views.py
@@ -1,3 +1,4 @@
+from pickle import FALSE
 from shutil import ExecError
 from django.shortcuts import redirect, render
 from django.http import HttpResponse, HttpResponseRedirect
@@ -6,6 +7,13 @@ from django.urls import reverse
 from django.core.exceptions import FieldError
 from django.db.models import Field
 from django.apps import apps
+import pickle
+import zipfile
+import os
+import pandas as pd
+import csv
+from model_classes import *
+import tqdm
 
 TABLES = [x.__name__ for x in apps.get_models() if not x.__name__.startswith('Auth') and not x.__name__.startswith('Django')][:-6]
 REQUEST_QUERY_KEY = 'query'
@@ -15,8 +23,90 @@ BUTTON_CLASSIFY_KEY = 'classify'
 BUTTON_CLUSTER_KEY = 'cluster'
 BUTTON_CANCEL_KEY = 'cancel'
 
+INITIALIZED = False
+
+BOOLEAN_MODEL = None
+TF_IDF_MODEL = None
+FASTTEXT_MODEL = None
+TRANSFORMER_MODEL = None
+LOGISTIC_REGRESSION_MODEL = None
+TRANSFORMER_CLASSIFICATION_MODEL = None
+CLUSTER_MODEL = None
+TSNE_MODEL = None
+TSNE_DATASET = None
+DATASET = None
+PREPROCESSED_TEXT = None
+MINI_4K_DATASET = None
+MINI_4K_PREPROCESSED_TEXT = None
+MINI_10K_DATASET = None
+MINI_10K_PREPROCESSED_TEXT = None
+PREPROCESSOR = Preprocessor(stopwords_path='models/stopwords.txt')
+
+def read_dataset_from_file():
+    dataset = []
+    with zipfile.ZipFile('dataset.zip', 'r') as zip_file:
+        zip_file.extractall()
+    with open('dataset.csv', encoding='utf-8') as file:
+        csv_reader = csv.reader(file)
+        header = next(csv_reader)
+        for row in csv_reader:
+            data = dict(zip(header[1:], row[1:]))
+            dataset.append(data)
+    os.remove('dataset.csv')
+    return dataset
+
+def data_to_text(data):
+    return ' '.join([data['title'], data['intro'], data['body']]).lower()
+
+def get_mini_dataset(len_each_category=400):
+    global CATEGORIES, dataset
+
+    mini_dataset = []
+    for category in CATEGORIES.keys():
+        dataset_by_category = dataset.loc[dataset['category'] == category]
+        length = min(len_each_category, dataset_by_category.shape[0])
+        mini_dataset.append(dataset_by_category.sample(length, random_state=1))
+
+    mini_dataset = pd.concat(mini_dataset).reset_index(drop=True)
+    texts = [data_to_text(data) for _, data in mini_dataset.iterrows()]
+    mini_preprocessed_texts = [PREPROCESSOR.preprocess(text) for text in tqdm(texts)]
+    return mini_dataset, mini_preprocessed_texts
+
+
+mini_dataset, mini_preprocessed_texts = get_mini_dataset()
+
+
+def init_models():
+    DATASET = read_dataset_from_file
+    with open('models/Preprocessed_texts.pickle', "rb") as file:
+        PREPROCESSED_TEXT = pickle.load(file)
+    MINI_4K_DATASET, MINI_4K_PREPROCESSED_TEXT = get_mini_dataset()
+    MINI_10K_DATASET, MINI_10K_PREPROCESSED_TEXT = get_mini_dataset(1000)
+    with open('models/BooleanIR_model.pickle', "rb") as file:
+            BOOLEAN_MODEL = pickle.load(file)
+    with open('models/TF_IDF_model.pickle', "rb") as file:
+            TF_IDF_MODEL = pickle.load(file)
+    with open('models/Transformer_model.pickle', "rb") as file:
+            TRANSFORMER_MODEL = pickle.load(file)
+    with open('models/Logistic_Regression.pickle', "rb") as file:
+            LOGISTIC_REGRESSION_MODEL = pickle.load(file)
+    with open('models/Transformer_classification.pickle', "rb") as file:
+            TRANSFORMER_CLASSIFICATION_MODEL = pickle.load(file)
+    with open('models/KMeans.pickle', "rb") as file:
+            CLUSTER_MODEL = pickle.load(file)
+    with open('models/TSNE_model.pickle', "rb") as file:
+            TSNE_MODEL = pickle.load(file)
+    with open('models/TSNE_dataset.pickle', "rb") as file:
+            TSNE_DATASET = pickle.load(file)
+    FASTTEXT_MODEL = FastText()
+    FASTTEXT_MODEL.prepare(MINI_4K_PREPROCESSED_TEXT, mode='load', save=False)
+    
+    
 
 def home(request):
+    if not INITIALIZED:
+        init_models()
+        INITIALIZED = True
     context = {
         'tables': TABLES,
         'header': [],
@@ -35,13 +125,22 @@ def home(request):
 
 
 def cluster(request):
-    if request.method == 'POST' and BUTTON_CANCEL_KEY in request.POST:
-        return redirect('mir-home')
+    if request.method == 'POST':
+        if BUTTON_CANCEL_KEY in request.POST:
+            return redirect('mir-home')
+        else:
+            query = request.POST.get(REQUEST_QUERY_KEY, None)
+    # plot dataset and query
+    # plt.to_html()
     return render(request, 'mir/cluster.html')
 
 def classify(request):
-    if request.method == 'POST' and BUTTON_CANCEL_KEY in request.POST:
-        return redirect('mir-home')
+    if request.method == 'POST':
+        if BUTTON_CANCEL_KEY in request.POST:
+            return redirect('mir-home')
+        else:
+            query = request.POST.get(REQUEST_QUERY_KEY, None)
+
     return render(request, 'mir/cluster.html')
 
 
